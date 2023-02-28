96f9ce2f723af9fb1e36d429fdc522ff16283011
R1dodke <117977613+R1dodke@users.noreply.github.com>
2023-02-28 20:54:42 +0530

    task no.7 sharpener

--- dom.js
+++ dom.js
@@ -1,5 +1,22 @@
-var secondItem= document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor='green';
+var newDiv=document.createElement('div');
+var newDivText= document.createTextNode('HEllo word');
+newDiv.appendChild(newDivText);
 
-var thirdItem= document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.color='white';
+var container= document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+console.log(newDiv);
+
+container.insertBefore(newDiv, h1);
+
+
+var newDiv2=document.createElement('div');
+var newDivText2= document.createTextNode('HEllo word');
+newDiv2.appendChild(newDivText2);
+
+var title= document.querySelector('ul .list-group');
+var h2=document.querySelector('ul h2');
+
+console.log(newDiv2);
+
+container.insertBefore(newDiv2, h2);
