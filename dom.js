b7cc29d026021d365bb37d782173372a8a82f57d
R1dodke <117977613+R1dodke@users.noreply.github.com>
2023-03-06 20:03:26 +0530

    task no.12 sharpener

--- dom.js
+++ dom.js
@@ -2,8 +2,25 @@ function test(){
     var username=document.getElementById('name').value;
     var email=document.getElementById('Email').value;
 
-    var uid=JSON.stringify(localStorage.setItem("username",username));
-    var email=JSON.stringify(localStorage.setItem("email", email));
+    let user_records = new Array();
+    user_records=JSON.parse(localStorage.getItem("user"))?JSON.parse(localStorage.getItem("user")):[]
+
+    if(user_records.some((v)=>{return v.email==email}))
+    {
+        alert("duplicate data");
+    }
+    else{
+        user_records.push({
+            "username":username,
+            "email":email,
+        })
+    
+        localStorage.setItem("user",JSON.stringify(user_records));
+
+    }
+
+   
+    
 
 }
 
